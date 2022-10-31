bf2a6d82ef002620390e9644d47009a045549971
Marco Aurélio Soares <marcosoares@ciandt.com>
2022-10-31 09:56:19 -0300

    [master] Adding exercise E5

---
+++ projects/exercicios-avulsos/e5-challenge-build-staircase/tests/index.test.js
@@ -0,0 +1,57 @@
+import { buildStairs } from '../index';
+
+describe('E5 - Challenge - Build staircase', () => {
+  const printResult = (result) => console.log(`Result:\n${result}`);
+
+  it('Should throw an error when number of steps are less than 2', () => {
+    try {
+      buildStairs(1);
+    } catch (err) {
+      expect(err.message).toEqual('No need to build stairs');
+      return;
+    }
+    throw new Error('Expected to fail, but this test was executed successfully.');
+  });
+
+  it('Should generate stairs correctly with 3 steps', () => {
+    const result = buildStairs(3);
+    printResult(result);
+
+    expect(result).toEqual(
+      '  #\n' +
+      ' ##\n' +
+      '###'
+    );
+  });
+
+  it('Should generate stairs correctly with 5 steps', () => {
+    const result = buildStairs(5);
+    printResult(result);
+
+    expect(result).toEqual(
+      '    #\n' +
+      '   ##\n' +
+      '  ###\n' +
+      ' ####\n' +
+      '#####'
+    );
+  });
+
+  it('Should generate stairs correctly with 10 steps', () => {
+    const result = buildStairs(10);
+    
+    printResult(result);
+    expect(result).toEqual(
+      '         #\n' +
+      '        ##\n' +
+      '       ###\n' +
+      '      ####\n' +
+      '     #####\n' +
+      '    ######\n' +
+      '   #######\n' +
+      '  ########\n' +
+      ' #########\n' +
+      '##########'
+    );
+  });
+});
